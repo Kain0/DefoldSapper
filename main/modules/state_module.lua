192bb27d36f902df8de6127381cd7e4f4fcab14c
Kain0 <andreypopov1603@gmail.com>
2022-10-30 08:04:38 +0300

    task done

---
+++ main/modules/state_module.lua
@@ -0,0 +1,9 @@
+local M = {}
+
+M.state = 0
+
+return M
+-- Put functions in this file to use them in several other scripts.
+-- To get access to the functions, you need to put:
+-- require "my_directory.my_file"
+-- in any script using the functions.
